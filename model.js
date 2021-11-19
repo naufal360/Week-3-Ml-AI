b0a58c9868fc10dbb9d79a7e003650743eea390a
naufal360 <naufal360.an@gmail.com>
2021-11-19 21:28:24 +0700

    benerin lagi model.js

--- model.js
+++ model.js
@@ -11,33 +11,33 @@ function loadData() {
 
 // Buat fungsi untuk load data disini
 async function loadModel(){
-  console.log('loading model...')
+  console.log('loading model....');
   model = await tf.loadLayersModel(
     'file://${__dirname}/model/model.json',
     false
-  )
-  console.log('Model loaded Succesfull')
-}
+  );
+  console.log('Model Loaded Successfull')
+};
 
 const movie_arr = tf.tensor(loadData());
 const movie_len = movies.length;
 
 // Buat fungsi untuk memberi rekomendasi film
 exports.recommend = async function recommend(userId){
-  let user = tf.fill([movie_len], Number(userId))
-  let movie_in_js_array = movie_arr.arraySync()
-  await loadModel()
-  console/log('Recommending for user: ${userId}')
-  pred_tensor = await model.predict([movie_arr, user]).reshape([movie_len])
-  pred = pred_tensor.arraySync()
+  let user = tf.fill([movie_len],Number(userId));
+  let movie_in_js_array = movie_arr.arraySync();
+  await loadModel();
+  console.log('Recommending for user: ${userId}');
+  pred_tensor = await model.predict([movie_arr,user]).reshape([movie_len]);
+  pred = pred_tensor.arraySync();
 
-  let recommendation = []
-  for (let i = 0; i<6; i++){
-    max = pred_tensor.argMax().arraySync()
-    recommendation.push(movies[max])
-    pred.splice(max,1)
-    pred_tensor = tf.sensor(pred)
+  let recommendations = [];
+  for (let i = 0;i<6;i++){
+    max = pred_tensor.argMax().arraySync();
+    recommendations.push(movies[max]);
+    pred.splice(max,1);
+    pred_tensor = tf.tensor(pred);
   }
-  console.log(recommendation)
-  return recommendation
+  console.log(recommendations)
+  return recommendations
 }
